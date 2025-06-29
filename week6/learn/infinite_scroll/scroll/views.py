b59ff77d65ec67bec9378f023589b77f1924b3f9
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-06-29 07:47:18 +0530

    completed week6 lecture

---
+++ week6/learn/infinite_scroll/scroll/views.py
@@ -0,0 +1,22 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+import time, json
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "scroll/index.html")
+
+def posts(request):
+    start = int(request.GET.get("start"))
+    end = int(request.GET.get("end"))
+
+    data = []
+
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    time.sleep(1)
+
+    return JsonResponse({"posts": data}) 
