dd846ff865893206aef0549b7530fa6b9d7c5756
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-07-24 06:01:37 +0530

    completed social media assignment

--- week7/project4/network/static/network/profile.js
+++ week7/project4/network/static/network/profile.js
@@ -71,7 +71,7 @@ function showPosts() {
         document.getElementById("prev_btn").style.display = page === 1 ? "none" : "inline-block";
 
         let profile_posts = document.querySelector("#profile_posts");
-        
+    
         data.posts.forEach((post) => {
             let postdiv = document.createElement("div");
             postdiv.className = "p-3 mb-3 border rounded shadow-sm";
@@ -98,12 +98,14 @@ function showPosts() {
                         }
                     </div>
                     <div class="pl-1"> ${ post.like_count }</div>
-                    <div id="editbtn" class="ml-4" style="cursor: pointer">
-                        Edit
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
-                        </svg>
-                    </div>
+                    ${
+                        post.author__username === currentuser ?
+                        `<div id="editbtn" class="ml-4" style="cursor: pointer">
+                            Edit
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+                            </svg>
+                        </div>` : ``}
                 </div>`;
                 postdiv.setAttribute('data-post-id', post.id);
             profile_posts.appendChild(postdiv);
